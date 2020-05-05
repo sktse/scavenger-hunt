9860a171e4f6c6873cb44f395ee8e31d3392b0ff
Stephen Tse <stephen.tse@goproperly.com>
2020-05-04 23:04:41 -0400

    Adds scavenger hunt generator.

---
+++ src/scavenger_hunt.py
@@ -0,0 +1,74 @@
+import random
+
+from typing import List
+
+KITCHEN_ITEMS = [
+    "Can Opener",
+    "Butter Knife",
+    "Chef's Knife",
+    "A Cup with a Logo",
+    "A Cup with Text",
+    "Pot",
+    "Pan",
+    "Sponge",
+    "Coffee or Tea",
+    "Dish Soap",
+    "Cooking Appliance",
+    "A Bottled or Canned Beverage",
+    "Spatula",
+    "Hot Sauce",
+    "Spice",
+    "Reusable Container",
+    "Serrated Knife",
+    "Alcohol",
+    "A Single Peppercorn",
+]
+
+OFFICE_ITEMS = [
+    "Blue Pen",
+    "Black Pen",
+    "Coloured Writing Device (not Blue or Black)",
+    "Mouse",
+    "USB Cable",
+    "Scissors",
+    "Sharpie",
+    "Notebook or Notepad",
+    "Headphones",
+    "Something Mailed",
+    "Box Cutter",
+    "A Piece of Paper with Your Name on it",
+    "A Phone",
+    "Memory Card or USB Stick",
+]
+
+LIVING_ITEMS = [
+    "Hat",
+    "Coat",
+    "Fitness Equipment",
+    "Pet or Plant",
+    "Photo in Frame",
+    "Disinfectant Wipes",
+    "Matching Pair of Socks",
+    "Fiction Book",
+    "Non-Fiction Book",
+    "Magazine",
+    "Board Game",
+    "Hardware Tool",
+    "Skin Care",
+    "Reusable Grocery Bag",
+    "Sun glasses",
+    "Gloves",
+    "Keys",
+    "A Remote Control",
+    "Pillow",
+]
+
+
+def pick_items() -> List[str]:
+    kitchen_items = random.sample(KITCHEN_ITEMS, k=5)
+    office_items = random.sample(OFFICE_ITEMS, k=5)
+    living_items = random.sample(LIVING_ITEMS, k=5)
+    combined_items = kitchen_items + office_items + living_items
+    random.shuffle(combined_items)
+
+    return combined_items
