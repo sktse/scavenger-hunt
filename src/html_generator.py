ba13b51fe8b97496d28422cd789851495efe7116
Stephen Tse <stephen.tse@goproperly.com>
2020-05-05 00:18:21 -0400

    Styles the output HTML.

--- src/html_generator.py
+++ src/html_generator.py
@@ -4,18 +4,20 @@ from typing import Iterable, List
 def generate_html(items: List[str]) -> str:
     args = [iter(items)] * 5
     data_rows = zip(*args)
-    table = "<html><table>\n"
+    html = "<html>\n"
+    html += "<h1>Scavenger Hunt List</h1>"
+    html += "<table width=\"100%\" border=\"1\" style=\"border-collapse:collapse\">\n"
     for data_row in data_rows:
-        table += generate_html_row(data_row)
-    table += "</table></html>\n"
+        html += generate_html_row(data_row)
+    html += "</table>\n</html>\n"
 
-    return table
+    return html
 
 
 def generate_html_row(items: Iterable[str]) -> str:
-    table_row = "  <tr>\n"
+    table_row = "  <tr height=\"200\">\n"
     for item in items:
-        table_row += f"    <td>{item}</td>\n"
+        table_row += f"    <td width=\"20%\" style=\"text-align:center;font-size:30px\">{item}</td>\n"
     table_row += "  </tr>\n"
 
     return table_row
--- tests/html_generator_tests.py
+++ tests/html_generator_tests.py
@@ -23,32 +23,12 @@ def items():
         "2001: A Space Odyssey",
     ]
 
+
 @pytest.fixture()
 def expected_html():
-    result = """<html><table>
-  <tr>
-    <td>Citizen Kane</td>
-    <td>The Godfather</td>
-    <td>Casablanca</td>
-    <td>Raging Bull</td>
-    <td>Singin' in the Rain</td>
-  </tr>
-  <tr>
-    <td>Gone with the Wind</td>
-    <td>Lawrence of Arabia</td>
-    <td>Schindler's List</td>
-    <td>Vertigo</td>
-    <td>The Wizard of Oz</td>
-  </tr>
-  <tr>
-    <td>City Lights</td>
-    <td>The Searchers</td>
-    <td>Star Wars</td>
-    <td>Psycho</td>
-    <td>2001: A Space Odyssey</td>
-  </tr>
-</table></html>
-"""
+    with open("./tests/expected_output.html", "r") as fs:
+        result = fs.read()
+
     return result
 
 
